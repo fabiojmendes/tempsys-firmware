c633ad692ce89b3a97d5b7923fedf19d830e2ea7
Fabio Mendes <fabiojmendes@gmail.com>
2024-09-30 17:10:14 -0400

    Update documentation

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
+<!-- vim: set tw=80: -->
+
 # Tempsys Firmware
 
-Bluetooth LE temperature sensor using embassy-rs
+Bluetooth LE temperature sensor using embassy-rs. This is the result of a
+direct port of a very [similar project](https://github.com/fabiojmendes/ble-temp-sensor)
+using Zephyr RTOS.
+
+This time around the language of choice is Rust and the [embassy](https://github.com/embassy-rs)
+ecosystem.
+
+## Overview
+
+The main purpose of this firmware is to interface with the MCP9808 sensor and
+emit Bluetooth LE advertising packets that can be collected by
+[tempsys-scan](https://github.com/fabiojmendes/tempsys-scan).
+
+## Advertising Packet Format
+
+| Manufacturer Id | Version | Counter | Voltage | Temperature |
+| --------------- | ------- | ------- | ------- | ----------- |
+| u16             | u8      | u8      | u16     | i16         |
+
+- Manufacturer Id is fixed to 0xFFFF for testing purposes.
+- Version of this packet format, currently 1.
+- This counter is incremented every time the firmware performs a reading. It
+  will wrap around once it spill over.
+- Voltage: 16 bit unsigned value of the battery voltage in millivolts.
+- Temperature: 16 bit signed value \* 2^10 of of the temperature in Celsius.
+
+> [!WARNING]
+> If the temperature reading is equal to `i16::MAX` an error has occurred.
 
 ## Steps for flashing
 
@@ -14,3 +43,8 @@ probe-rs erase --chip nrf52840_xxAA
 probe-rs download --verify --binary-format hex --chip nRF52840_xxAA s113_nrf52_7.X.X_softdevice.hex
 probe-rs run --chip nrf52840_xxAA tempsys-firmware
 ```
+
+## TODO
+
+- Migrate to postcard for serialization and return a proper Result instead of
+  `i16::MAX` to denote errors.
--- src/temperature.rs
+++ src/temperature.rs
@@ -6,6 +6,9 @@ use embassy_time::Timer;
 use crate::constants::{SAMPLE_RATE, WAKEUP_DELAY};
 
 const MCP9808_ADDRESS: u8 = 0x18;
+const CONFIG_REG: u8 = 0x01;
+const TEMPERATURE_REG: u8 = 0x05;
+const RESOLUTION_REG: u8 = 0x08;
 
 static SHARED: Signal<ThreadModeRawMutex, i16> = Signal::new();
 
@@ -39,22 +42,24 @@ pub async fn init(mut twi: Twim<'static, impl twim::Instance>) -> ! {
 
 async fn setup_temp_reader(twi: &mut Twim<'_, impl twim::Instance>) -> Result<(), twim::Error> {
     defmt::debug!("Set resolution");
-    twi.write(MCP9808_ADDRESS, &[0x08, 0x00]).await?;
+    twi.write(MCP9808_ADDRESS, &[RESOLUTION_REG, 0x00]).await?;
     Ok(())
 }
 
 async fn read_temperature(twi: &mut Twim<'_, impl twim::Instance>) -> Result<i16, twim::Error> {
     // Temp Sensor
     // Wake up
-    twi.write(MCP9808_ADDRESS, &[0x01, 0x00, 0x00]).await?;
+    twi.write(MCP9808_ADDRESS, &[CONFIG_REG, 0x00, 0x00])
+        .await?;
     Timer::after(WAKEUP_DELAY).await;
     // Read
     let mut buf = [0u8; 2];
-    twi.write_read(MCP9808_ADDRESS, &[0x05], &mut buf).await?;
+    twi.write_read(MCP9808_ADDRESS, &[TEMPERATURE_REG], &mut buf)
+        .await?;
     // Conversion code based on the datasheet
     // https://ww1.microchip.com/downloads/en/DeviceDoc/25095A.pdf pg25
     let [upper, lower] = buf;
-    let temp_raw = signed_12bit(upper, lower);
+    let temp_raw = raw_to_signed(upper, lower);
     let temperature = (temp_raw as f32 / 16.0 * 100.0) as i16;
     defmt::debug!(
         "Temperature: {} (upper: {:#02x}, lower: {:#02x})",
@@ -63,13 +68,16 @@ async fn read_temperature(twi: &mut Twim<'_, impl twim::Instance>) -> Result<i16
         lower
     );
     // Shutdown
-    twi.write(MCP9808_ADDRESS, &[0x01, 0x01, 0x00]).await?;
+    twi.write(MCP9808_ADDRESS, &[CONFIG_REG, 0x01, 0x00])
+        .await?;
     Ok(temperature)
 }
 
 /// Converts upper and lower u8 bytes to a signed 12 bit i16
-fn signed_12bit(upper: u8, lower: u8) -> i16 {
+fn raw_to_signed(upper: u8, lower: u8) -> i16 {
     // clear any bits after bit 11 and shift;
+    // combine with the lower 8 bits
     let val = ((upper & 0x1f) as i16) << 8 | lower as i16;
+    // shift to apply the signal bit to the i16
     (val << 4) >> 4
 }
